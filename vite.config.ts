d6b02a488f575f88323e9d6cb29743d0c0a363a9
Drswith <540628938@qq.com>
2025-08-31 19:21:37 +0000

    ci(github-pages): 添加 GitHub Pages 部署工作流
    新增 GitHub Pages 部署工作流配置文件，用于自动构建和部署静态内容到 GitHub Pages。配置包括设置 pnpm、Node.js 环境、安装依赖、构建项目并上传到 Pages。
    
    feat(vite): 支持动态配置 base 路径
    
    修改 vite.config.ts 以支持通过环境变量动态配置 base 路径，并增加构建时的 chunk 大小警告限制。

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import process from 'node:process'
+import { defineConfig, loadEnv } from 'vite'
 import usbIdsPlugin from './plugins/plugin-usb-ids'
 
-export default defineConfig({
-  plugins: [usbIdsPlugin({
-    fallbackFile: path.resolve(__dirname, 'usb.ids.json'),
-    skipInDev: true,
-  })],
+export default defineConfig(({ command }) => {
+  const env = loadEnv(command, process.cwd())
+
+  const base = env.VITE_BASE_PATH || '/'
+  console.log('vite base ', base)
+
+  return {
+    base,
+    plugins: [
+      usbIdsPlugin({
+        fallbackFile: path.resolve(__dirname, 'usb.ids.json'),
+        skipInDev: true,
+      }),
+    ],
+    build: {
+      chunkSizeWarningLimit: 2048,
+    },
+  }
 })
