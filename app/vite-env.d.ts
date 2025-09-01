2d9adc2e357495b5051e6f83e427ca420cccc0da
Drswith <540628938@qq.com>
2025-09-01 09:54:53 +0000

    refactor: 移除未使用的virtual:usb-ids模块声明

--- app/vite-env.d.ts
+++ app/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-// 声明虚拟模块
-declare module 'virtual:usb-ids' {
-  import type { UsbIdsData } from '../plugins/plugin-usb-ids/typing'
-
-  const usbIdsData: UsbIdsData
-  export default usbIdsData
-}
